6b9a8f70716a17ff067ba51423666c260d671e5e
Sydney Charles <68644060+sydneycharles@users.noreply.github.com>
2023-03-08 16:11:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var portfolioCollection = [
+
+	{
+		"name": "Music Player",
+		"info": "Front-end : Javascript, CSS and HTML",
+		"url": ""
+	},
+
+	{
+		"name": "NASA APOD",
+		"info": "NASA picture of a day using javascript and NASA Api",
+		"url": ""
+	},
+	{
+		"name": "Blur Loading Animation",
+		"info": "Front-end using vanilla Javascript, CSS and HTML",
+		"url": ""
+	},
+		{
+		"name": "Vynil Player",
+		"info": "NASA picture of a day using javascript and NASA Api",
+		"url": ""
+	},
+	{
+		"name": "Kanban Board",
+		"info": "Front-end using vanilla Javascript, CSS and HTML",
+		"url": ""
+	},
+	{
+		"name": "Theme Clock",
+		"info": "Front-end using vanilla Javascript, CSS and HTML",
+		"url": ""
+	}
+	
+];
+
+
+var vmPortfolio = new Vue({
+	el: "#vue-portfolio",
+	data: {
+		portfolios: portfolioCollection
+	}
+});
